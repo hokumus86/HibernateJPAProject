b09195825f41a42efd3c8e308608756303042c0b
hokumus86 <vektorel@lab9-ogr>
2018-12-07 20:11:06 +0200

    Login frame  created and first user adding  table from datebase.

---
+++ src/dersane/hokumus/proje/base/Runner.java
@@ -0,0 +1,12 @@
+package dersane.hokumus.proje.base;
+
+import dersane.hokumus.ui.kullanici.GirisFrm;
+
+public class Runner {
+	
+	public static void main(String[] args) {
+		GirisFrm frm = new  GirisFrm();
+		frm.setVisible(true);
+	}
+
+}
---
+++ src/dersane/hokumus/ui/kullanici/GirisFrm.java
@@ -0,0 +1,104 @@
+package dersane.hokumus.ui.kullanici;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+
+import dersane.hokumus.dao.KullaniciDAO;
+import dersane.hokumus.model.kullanici.Kullanici;
+import dersane.hokumus.model.kullanici.Rol;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.util.Date;
+import java.util.List;
+
+public class GirisFrm extends JFrame {
+	private JTextField txtKulAdi;
+	private JTextField txtSifre;
+	KullaniciDAO db = new KullaniciDAO();
+
+	public GirisFrm() {
+		setBounds(300, 300, 340, 240);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		getContentPane().setLayout(null);
+		setTitle("Dersane Uygulamasý Giriþ Ekraný");
+
+		JLabel lblKullaniciAdi = new JLabel("Kullanici Adi");
+		lblKullaniciAdi.setBounds(42, 91, 90, 14);
+		getContentPane().add(lblKullaniciAdi);
+
+		JLabel lblifre = new JLabel("\u015Eifre");
+		lblifre.setBounds(42, 116, 90, 14);
+		getContentPane().add(lblifre);
+
+		txtKulAdi = new JTextField();
+		txtKulAdi.setBounds(142, 88, 142, 20);
+		getContentPane().add(txtKulAdi);
+		txtKulAdi.setColumns(10);
+
+		txtSifre = new JTextField();
+		txtSifre.setBounds(142, 113, 142, 20);
+		getContentPane().add(txtSifre);
+		txtSifre.setColumns(10);
+
+		JButton btnIptal = new JButton("\u0130ptal");
+		btnIptal.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		btnIptal.setBounds(41, 155, 91, 23);
+		getContentPane().add(btnIptal);
+
+		JButton btnGiri = new JButton("Giri\u015F");
+		btnGiri.setBounds(179, 155, 91, 23);
+		getContentPane().add(btnGiri);
+
+		btnGiri.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				List<Kullanici> liste = db.listele(new Kullanici());
+				if (liste.size() <= 0) {
+					kullaniciEkle(liste);
+					liste = db.listele(new Kullanici());
+				}
+				for (Kullanici k : liste) {
+					if (k.getKullaniciAdi().equals(txtKulAdi.getText())
+							&& k.getSifre().equals(txtSifre.getText())) {
+						JOptionPane.showMessageDialog(GirisFrm.this,
+								"Giriþ Baþarýlý...!");
+					} else {
+						JOptionPane.showMessageDialog(GirisFrm.this,
+								"Giriþ Baþarýsýz...!");
+					}
+
+				}
+
+			}
+
+		});
+
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(
+				"C:\\Users\\vektorel\\Desktop\\logo.png"));
+		lblNewLabel.setBounds(67, 11, 242, 74);
+		getContentPane().add(lblNewLabel);
+	}
+
+	private void kullaniciEkle(List<Kullanici> liste) {
+		Kullanici temp = new Kullanici();
+		temp.setAd("Hüseyin");
+		temp.setEmail("lgsdklsdjfdj");
+		temp.setKullaniciAdi("hokumus");
+		temp.setRol(Rol.ADMIN);
+		temp.setSifre("1");
+		temp.setSoyad("Okumuþ");
+		temp.setUyelikTarihi(new Date());
+		db.kaydet(temp);
+
+	}
+}
